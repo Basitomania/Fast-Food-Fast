fa8ffefbe2e752114df88396dd25d4b56c0c8a6d
Basitomania <basitabdulkareeem@gmail.com>
2018-09-01 02:36:52 +0100

    changes to the homepage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-function classToggle() {
+classToggle = () => {// toggle function for mobile responsiveness
     const navs = document.querySelectorAll('.Navbar-Items')
 
     navs.forEach(nav => nav.classList.toggle('Navbar-ToggleShow'));
 }
-document.querySelector('.Navbar-Link-toggle')
-    .addEventListener('click', classToggle);
+document.querySelector('.Navbar-Link-toggle').addEventListener('click', classToggle);
+// signup form
+// Get the modal
+var modal = document.getElementById('signup');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
