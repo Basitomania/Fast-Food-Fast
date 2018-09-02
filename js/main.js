a5a282ef13ddb1a7272c14c7ebb87b0cbeb699c1
Basitomania <basitabdulkareeem@gmail.com>
2018-09-02 12:21:22 +0100

    continious changes to the main page to improve responsiveness

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-classToggle = () => {// toggle function for mobile responsiveness
+// toggle function for mobile responsiveness
+classToggle = () => { 
     const navs = document.querySelectorAll('.Navbar-Items')
 
     navs.forEach(nav => nav.classList.toggle('Navbar-ToggleShow'));
 }
 document.querySelector('.Navbar-Link-toggle').addEventListener('click', classToggle);
+
 // signup form
 // Get the modal
-var modal = document.getElementById('signup');
+var modalUp = document.getElementById('signup');
+var modalIn = document.getElementById('login');
+var close = document.getElementById('close');
+var signupLink = document.getElementById('signpLink');
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+// When the user clicks anywhere outside of the modal, close it
+hide = (e) => {
+    if (e.target == modalUp || e.target == modalIn) {
+        modalUp.style.display = "none";
+        modalIn.style.display = "none";
+    }
+    if (e.target == close || e.target == close) {
+        modalUp.style.display = "none";
+        modalIn.style.display = "none";
+    }
+}
+
+modalUp.addEventListener('click', hide);
+modalIn.addEventListener('click', hide);
