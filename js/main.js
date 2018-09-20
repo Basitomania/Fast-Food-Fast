b4bc6f5a8221f4bb610677b87ced9acbaa122a80
Basitomania <basitabdulkareeem@gmail.com>
2018-09-20 23:01:00 +0100

    fixed mobile resposiveness

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // toggle function for mobile responsiveness
-classToggle = () => { 
+function classToggle() { 
     const navs = document.querySelectorAll('.Navbar-Items')
 
     navs.forEach(nav => nav.classList.toggle('Navbar-ToggleShow'));
 }
-document.querySelector('.Navbar-Link-toggle').addEventListener('click', classToggle);
+document.querySelector('.Navbar-Link-toggle')
+.addEventListener('click', classToggle);
 
 // signup form
 // Get the modal
